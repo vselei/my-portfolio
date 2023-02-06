daebc47f8c8553d8e272b24914b8139a643904aa
Victor Seleimend <vsennaselei@gmail.com>
2023-02-06 18:53:37 -0300

    estilizando ao dar hover na imagem

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,16 +7,28 @@ import profileImg from '/img/profile.webp';
 
 const ProfileImg = () => {
   const [name, setName] = useState(false);
+  const [clickMessage, setClickMessage] = useState(false);
 
   return (
     <div className={`${styles.profile} flex align-center`}>
-      <img
-        onClick={() => setName(!name)}
-        width="60"
-        height="60"
-        src={profileImg}
-        alt="Victor Seleimend"
-      />
+      <div className={styles['img-container']}>
+        {clickMessage && (
+          <p
+            onClick={() => setName(!name)}
+            onMouseLeave={() => setClickMessage(false)}
+            className={`${styles.message} flex align-center justify-center`}
+          >
+            Clique!
+          </p>
+        )}
+        <img
+          onMouseMove={() => setClickMessage(true)}
+          width="60"
+          height="60"
+          src={profileImg}
+          alt="Victor Seleimend"
+        />
+      </div>
       {name ? <ProfileName /> : <Arrows />}
     </div>
   );
