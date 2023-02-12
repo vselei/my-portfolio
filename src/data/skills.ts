899b7a074edf41f09a3833b5b4f1fa0d01f0bcd5
Victor Seleimend <vsennaselei@gmail.com>
2023-02-12 20:25:14 -0300

    separando os dados

---
+++ src/data/skills.ts
@@ -0,0 +1,48 @@
+const cards: Array<{ title: string; image: string }> = [
+  {
+    title: 'Autodidata',
+    image: 'books.svg'
+  },
+  {
+    title: 'Inteligência emocional',
+    image: 'brain.svg'
+  },
+  {
+    title: 'Confiança',
+    image: 'shield.svg'
+  },
+  {
+    title: 'Resolução de problemas',
+    image: 'cube.svg'
+  },
+  {
+    title: 'Analítico',
+    image: 'analytics.svg'
+  },
+  {
+    title: 'Resiliência',
+    image: 'resilience.svg'
+  },
+  {
+    title: 'Criativo',
+    image: 'light.svg'
+  },
+  {
+    title: 'Empático',
+    image: 'empathy.svg'
+  },
+  {
+    title: 'Otimista',
+    image: 'optimism.svg'
+  },
+  {
+    title: 'Entusiasta',
+    image: 'gym.svg'
+  },
+  {
+    title: 'Focado',
+    image: 'focus.svg'
+  }
+];
+
+export default cards;
