d288800d6968e69b2706de518a7e5e7da163241a
Victor Seleimend <vsennaselei@gmail.com>
2023-02-18 15:04:19 -0300

    some fixes

--- src/components/ProfileName.tsx
+++ src/components/ProfileName.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Profile.module.css';
 const ProfileName = () => {
   return (
     <a href='/'>
-      <h1 className={`${styles.profileName} heading-two`}>Victor Seleimend</h1>
+      <h1 className={`${styles.profileName} heading-two`}>myPortfolio</h1>
     </a>
   );
 };
